275939682da98a2860c6dd0d64a9b8be989eafaf
Andrew Straw <strawman@astraw.com>
2015-02-28 05:40:07 +0100

    serve file

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.2');
-  api.addFiles('strawlab:meteor-tiff.js');
+  api.addFiles('tiff.min.js');
 });
 
 Package.onTest(function(api) {
