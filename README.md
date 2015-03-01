00add5b89b0c6eb7d7aa3ed1f2dba9bed3d77d12
Andrew Straw <strawman@astraw.com>
2015-03-01 12:19:52 +0100

    update documentation

---
+++ README.md
@@ -0,0 +1,2 @@
+This is [meteor](https://meteor.com) wrapper for
+[seikichi/tiff.js](https://github.com/seikichi/tiff.js).
--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'strawlab:meteor-tiff',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'A meteor wrapper for seikichi/tiff.js',
+  git: 'https://github.com/strawlab/strawlab-meteor-tiff.git',
   documentation: 'README.md'
 });
 
@@ -16,7 +12,4 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('strawlab:meteor-tiff');
-  api.addFiles('strawlab:meteor-tiff-tests.js');
 });
