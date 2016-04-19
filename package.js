b8d3f68db7fbaf245e4a724091bfd7f71f29109a
Andrew Straw <strawman@astraw.com>
2016-04-19 11:42:04 +0200

    bump version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'strawlab:meteor-tiff',
-  version: '0.0.1',
+  version: '0.1.0',
   summary: 'A meteor wrapper for seikichi/tiff.js',
   git: 'https://github.com/strawlab/strawlab-meteor-tiff.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.2');
+  api.versionsFrom('1.3.2.2');
   api.addFiles('tiff.min.js');
 });
 
